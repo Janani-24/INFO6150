7506daff324ddb776c6fc52665a33dbe44222e2f
Janani-24 <114230940+Janani-24@users.noreply.github.com>
2022-11-20 23:25:21 -0500

    Update Login.js

--- Assignment 9/Assignment 9 - frontend/react_spa/src/Login.js
+++ Assignment 9/Assignment 9 - frontend/react_spa/src/Login.js
@@ -1,87 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const { email, password } = this.state;
-    console.log(email, password);
-    fetch("http://localhost:8000/user/login", {
-      method: "POST",
-      crossDomain: true,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data, "userRegister");
-        if (data.status === "ok") {
-          alert("login successful");
-          window.localStorage.setItem("token", data.data);
-          window.location.href = "./home";
-        }
-      });
-  }
-  render() {
+const LOGIN_URL = 'http://localhost:8000/user/login';
+
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [credErrors, setcredErrors] = useState("");
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+            let userLoginData = {
+                emailId: email,
+                password: password,
+            };
+            try {
+                let response = await axios.post(LOGIN_URL, userLoginData);
+                if (response.status === 200) {
+                    sessionStorage.setItem("name", response.data.result.name)
+                    navigate("/home");
+                }
+                else{
+                    console.log(response.message);
+                }
+            }
+            catch (error) {
+                setcredErrors("Invalid credentials");
+            }
+        
+    }
+
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Sign In</h3>
-
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={(e) => this.setState({ email: e.target.value })}
-          />
+        <div className="container">
+            <div className="row">
+                <div className="col-md-5 mx-auto">
+                    <div className="card mt-5">
+                        <div className="card-header" style={{backgroundColor: "#0d6efd"}}>
+                            <h3 className="mb-3" style={{color: "white"}}>Login</h3>
+                        </div>
+                        <div className="card-body" style={{backgroundColor: "#ffffff"}}>
+                            <form id="loginForm" onSubmit={e => handleSubmit(e) }>
+                                <div className="form-group mt-4">
+                                    <label className="float-start mb-3">Email Id: </label>
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Enter email id"
+                                      defaultValue="test@gmail.com" required/>
+                                </div>
+                                <div className="form-group mt-4">
+                                    <label className="float-start mb-3">Password: </label>
+                                    <input type="password" className="form-control" name="pwd" id="pwd" placeholder="Enter password"
+                                    defaultValue="*********" required/>
+                                </div>
+                                <button type="submit" className="btn btn-primary mt-4">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-        </div>
-
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  }
+    )
 }
 
 export default Login;
